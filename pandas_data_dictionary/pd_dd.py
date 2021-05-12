fbfdc57073f9210390e7d03f6f0e3cf114503a86
tcouch <t.couch@ucl.ac.uk>
2021-05-12 08:57:47 +0100

    passed access datatypes property

--- pandas_data_dictionary/pd_dd.py
+++ pandas_data_dictionary/pd_dd.py
@@ -4,3 +4,13 @@ import pandas as pd
 class DataDictionaryAccessor():
     def __init__(self, pandas_obj):
         self._df = pandas_obj
+        self.reset_data_dict()
+
+    def reset_data_dict(self):
+        self._data_dict = pd.DataFrame()
+        self._data_dict.reindex(self._df.columns)
+        self._data_dict['datatype'] = self._df.dtypes.astype(str)
+
+    @property
+    def datatypes(self):
+        return self._data_dict['datatype']
