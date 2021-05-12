9eff0c4690ba989a39531da99d69d1085f392de8
tcouch <t.couch@ucl.ac.uk>
2021-05-12 11:57:59 +0100

    passed set title; refactored with set_var_property

--- pandas_data_dictionary/pd_dd.py
+++ pandas_data_dictionary/pd_dd.py
@@ -19,11 +19,24 @@ class DataDictionaryAccessor():
     def datatype(self):
         return self._data_dict['datatype']
 
+    def set_var_property(self,var:str,property_column:str,value,dtype=str):
+        # Check if column exists. If not create it with empty value of appropriate type
+        if property_column not in self._data_dict:
+            # Hopefully this type checking is sufficient for all use cases
+            empty_value = "" if dtype==str else np.NaN 
+            self._data_dict[property_column] = empty_value
+        self._data_dict.at[var,property_column] = value
+
     @property
     def description(self):
         return self._data_dict['description']
 
     def set_desc(self,var:str,desc:str):
-        if 'description' not in self._data_dict:
-            self._data_dict['description'] = ""
-        self._data_dict.at[var,'description'] = desc
+        self.set_var_property(var,'description',desc)
+
+    @property
+    def title(self):
+        return self._data_dict['title']
+
+    def set_title(self,var:str,title:str):
+        self.set_var_property(var,'title',title)
